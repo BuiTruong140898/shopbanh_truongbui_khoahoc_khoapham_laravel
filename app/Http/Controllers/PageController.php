d0d402129341d6cd76a5fcb271ffc10fcf5d61a5
your username <buitruong140898@gmail.com>
2019-05-13 00:43:44 +0700

    make models and design layout

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Slide;
+
+use App\Product;
+
+class PageController extends Controller
+{
+    public function getIndex(){
+        $slide = Slide::all();
+        $new_product = Product::where('new',1)->paginate(8);
+        $promotion_product = Product::where('promotion_price','<>',0)->paginate(4);
+    	return view('page.trangchu',compact('slide','new_product','promotion_product'));
+    }
+
+    public function getLoaiSP(){
+    	return view('page.loai_sanpham');
+    }
+
+    public function getChiTietSP(){
+    	return view('page.chitiet_sanpham');
+    }
+
+    public function getLienHe(){
+    	return view('page.lienhe');
+    }
+
+    public function getGioiThieu(){
+    	return view('page.gioithieu');
+    }
+}
+
---
+++ resources/views/page/trangchu.blade.php
@@ -0,0 +1,139 @@
+@extends('master')
+@section('content')	
+	<div class="rev-slider">
+		<div class="fullwidthbanner-container">
+					<div class="fullwidthbanner">
+						<div class="bannercontainer" >
+					    <div class="banner" >
+								<ul>
+									<!-- THE FIRST SLIDE -->
+									@foreach($slide as $sl)
+									<li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+													<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="source/image/slide/{{$sl->image}}" data-src="source/image/slide/{{$sl->image}}" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+													</div>
+												</div>
+
+							        </li>
+									@endforeach
+								
+								</ul>
+							</div>
+						</div>
+
+						<div class="tp-bannertimer"></div>
+					</div>
+		</div>
+				<!--slider-->
+	</div>
+	<div class="container">
+		<div id="content" class="space-top-none">
+			<div class="main-content">
+				<div class="space60">&nbsp;</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="beta-products-list">
+							<h4>Sản phẩm mới</h4>
+							<div class="beta-products-details">
+								<p class="pull-left">Tìm thấy {{count($new_product)}} sản phẩm</p>
+								<div class="clearfix"></div>
+							</div>
+
+							<div class="row">
+								
+								@foreach($new_product as $new)
+								
+								<div class="col-sm-3">
+									<div class="single-item">
+									@if($new->promotion_price)	
+										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+									@endif
+										<div class="single-item-header">
+											<a href="product.html"><img height="320px" src="source/image/product/{{$new->image}}" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">{{$new->name}}</p>
+											<p class="single-item-price">
+												@if($new->promotion_price)
+													<span class="flash-del">{{$new->unit_price}}đ</span>
+													<span class="flash-sale">{{$new->promotion_price}}đ</span>
+													
+												@else
+													<span class="flash-sale">{{$new->unit_price}}đ</span>
+												@endif
+
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="newduct.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+
+								@endforeach
+
+							</div>
+
+							<div class='row'>{{$new_product->links()}}</div>
+
+						</div> <!-- .beta-products-list -->
+
+						<div class="space50">&nbsp;</div>
+
+						<div class="beta-products-list">
+							<h4>Sản phẩm khuyến mãi</h4>
+							<div class="beta-products-details">
+								<p class="pull-left">Tìm thấy {{count($promotion_product)}} sản phẩm</p>
+								<div class="clearfix"></div>
+							</div>
+							<div class="row">
+								@foreach($promotion_product as $promo)
+
+									<div class="col-sm-3">
+									<div class="single-item">
+									@if($promo->promotion_price)	
+										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+									@endif
+										<div class="single-item-header">
+											<a href="product.html"><img height="320px" src="source/image/product/{{$promo->image}}" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">{{$promo->name}}</p>
+											<p class="single-item-price">
+												@if($promo->promotion_price)
+													<span class="flash-del">{{$promo->unit_price}}đ</span>
+													<span class="flash-sale">{{$promo->promotion_price}}đ</span>
+													
+												@else
+													<span class="flash-sale">{{$promo->unit_price}}đ</span>
+												@endif
+
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="newduct.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+
+									@endforeach
+
+								</div>
+								
+								<div class='row'>{{$promotion_product->links()}}</div>
+
+							</div>
+							
+						</div> <!-- .beta-products-list -->
+					</div>
+				</div> <!-- end section with sidebar and main content -->
+
+
+			</div> <!-- .main-content -->
+		</div> <!-- #content -->
+	</div> <!-- .container -->
+@endsection
